eee87ee02678f2ec75791e3c3452f66892bd40df
wildson <wildsonifb@gmail.com>
2024-05-23 20:29:55 -0300

    1.0

---
+++ main.py
@@ -0,0 +1,16 @@
+#criar lista de nomes 
+#lista 
+nomes=['Aléx', 'Simone', 'Bermardo','César','Alexandra']
+
+#exibe a lista na tela
+#print(nomes)
+
+
+#for i in range(len(nomes)):
+    #print(f'{i + 1}º nome da lista: {nomes[i]}.')
+#coloca em ordem alfabetica 
+nomes.sort()
+
+
+for nome in nomes:
+    print(nome)
